12908a24bc2a84f38c6f26d155dbec591140a6b1
lifeblood123 <zhangyuner112@163.com>
2024-03-25 15:18:06 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,23 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: "development",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'webpack-numbers.js',
     library: {
         name: 'webpackNumbers',
         type: 'umd',
-      }  
-  },
-  externals: {
-    lodash: {
-      commonjs: 'lodash',
-      commonjs2: 'lodash',
-      amd: 'lodash',
-      root: '_',
-    },
-  },
+      },
+      // libraryTarget: 'umd'
+  }
+  // ,
+  // externals: {
+  //   lodash: {
+  //     commonjs: 'lodash',
+  //     commonjs2: 'lodash',
+  //     amd: 'lodash',
+  //     root: '_',
+  //   },
+  // },
 };
