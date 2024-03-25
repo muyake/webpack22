79d9d87988f06793ae40f96918cfef61ccfb0856
lifeblood123 <zhangyuner112@163.com>
2024-03-25 21:16:10 +0800

    抽离css

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -3,6 +3,7 @@ const webpack = require("webpack");
 const minifyPrivatesTransformer = require('ts-transformer-minify-privates').default;
 let isProduction = process.env.NODE_ENV === 'production';
 console.log('Production: ', process.env.NODE_ENV); // true
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
@@ -36,8 +37,9 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // 将 JS 字符串生成为 style 节点
-          'style-loader',
+          // // 将 JS 字符串生成为 style 节点
+          // 'style-loader',
+          MiniCssExtractPlugin.loader, 
           // 将 CSS 转化成 CommonJS 模块
           'css-loader',
           // 将 Sass 编译成 CSS
@@ -49,7 +51,10 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       '__DEV__': JSON.stringify(!isProduction),
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].min.css",
+    }),
   ],
   resolve: {
     alias: {
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,24 @@
 const { merge } = require("webpack-merge");
 const base = require('./webpack.config.base');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = merge(base, {
   mode: "production",
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: '[name].min.js',
+  //   // publicPaht: 'http://baidu.com',
+  //   library: {
+  //       name: 'mtdpMap',
+  //       type: 'umd',
+  //     },
+  //     libraryTarget: 'umd'
+  // },
+  plugins:[
+    new MiniCssExtractPlugin({
+      filename: "[name].min.css",
+    }),
+  ]
+
 })
 module.exports = config;
