8be685d5c546d9353e69f7b8392d87d9977ec5be
lifeblood123 <zhangyuner112@163.com>
2024-03-25 16:13:19 +0800

    map

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@
 import './style.css';
 import printMe from './print.js';
 import _ from 'lodash';
+import * as mtdpMap from "webpackstduy22";
+
 function component() {
+    const map = new mtdpMap.Map();
+    console.log(map.getCenter());
     const element = document.createElement('div');
     const btn = document.createElement('button');
-  
-     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    btn.innerHTML = '1113Click me and check the console!';
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // const nn = webpackNumbers.Add(4, 3);
+    // console.log(nn);
+    // console.log(webpackNumbers);
+    btn.innerHTML = 11;
     btn.onclick = printMe;
-  
+
     element.appendChild(btn);
-  
-     return element;
-  }
-  
-  document.body.appendChild(component());
+
+    return element;
+}
+
+document.body.appendChild(component());
